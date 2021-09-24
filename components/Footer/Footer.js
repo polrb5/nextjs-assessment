d76c67542945125df985e54b4226ae3b04aced98
Pol Reig <83955122+polrb5@users.noreply.github.com>
2021-09-24 09:00:52 +0200

    Add select languages in Footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,3 +1,12 @@
+import styles from "./Footer.module.css";
+
 export default function Footer() {
-  return <footer className="">FOOTER</footer>;
+  return (
+    <footer className={styles.footer}>
+      <select className={styles.selectLang} name="language" id="lang">
+        <option value="english">English</option>
+        <option value="spanish">Spanish</option>
+      </select>
+    </footer>
+  );
 }
--- components/MainSection/MainSection.js
+++ components/MainSection/MainSection.js
@@ -5,35 +5,31 @@ export default function MainSection() {
   return (
     <main className={styles.containerSection}>
       <section className={styles.section}>
-        <div className={`${styles.sectionItem} ${styles.sectionText1}`}>
-          <div className={styles.card}>
-            <h2>Jellyfish</h2>
-            <Image
-              src="/jellyfish1.jpg"
-              alt="A jellyfish"
-              width="250"
-              height="250"
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
-              impedit numquam atque dolorum eos eaque.
-            </p>
-          </div>
+        <div className={styles.card}>
+          <h2>Jellyfish</h2>
+          <Image
+            src="/jellyfish1.jpg"
+            alt="A jellyfish"
+            width="250"
+            height="250"
+          />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem impedit
+            numquam atque dolorum eos eaque.
+          </p>
         </div>
-        <div className={`${styles.sectionItem} ${styles.sectionText1}`}>
-          <div className={styles.card}>
-            <h2>Jellyfish</h2>
-            <Image
-              src="/jellyfish2.jpg"
-              alt="A few jellyfish"
-              width="250"
-              height="250"
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
-              impedit numquam atque dolorum eos eaque.
-            </p>
-          </div>
+        <div className={styles.card}>
+          <h2>Jellyfish</h2>
+          <Image
+            src="/jellyfish2.jpg"
+            alt="A few jellyfish"
+            width="250"
+            height="250"
+          />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem impedit
+            numquam atque dolorum eos eaque.
+          </p>
         </div>
       </section>
     </main>
