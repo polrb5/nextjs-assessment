095d757c7babfe178c5f281486acfdadc16d8a86
Pol Reig <83955122+polrb5@users.noreply.github.com>
2021-09-27 09:26:08 +0200

    Add more translations

--- components/MainSection/MainSection.js
+++ components/MainSection/MainSection.js
@@ -1,35 +1,32 @@
 import Image from "next/image";
+import useTranslation from "../../hooks/useTranslation";
 import styles from "./MainSection.module.css";
 
 export default function MainSection() {
+  const { t } = useTranslation();
+
   return (
     <main className={styles.containerSection}>
       <section className={styles.section}>
         <div className={styles.card}>
-          <h2>Jellyfish</h2>
+          <h2>{t("jellyfish")}</h2>
           <Image
             src="/jellyfish1.jpg"
             alt="A jellyfish"
             width="250"
             height="250"
           />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem impedit
-            numquam atque dolorum eos eaque.
-          </p>
+          <p>{t("descriptionJellyfish")}</p>
         </div>
         <div className={styles.card}>
-          <h2>Jellyfish</h2>
+          <h2>{t("jellyfish")}</h2>
           <Image
             src="/jellyfish2.jpg"
             alt="A few jellyfish"
             width="250"
             height="250"
           />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem impedit
-            numquam atque dolorum eos eaque.
-          </p>
+          <p>{t("descriptionJellyfish")}</p>
         </div>
       </section>
     </main>
--- pages/about/index.js
+++ pages/about/index.js
@@ -2,14 +2,17 @@ import Link from "next/link";
 import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
 import MainSection from "../../components/MainSection/MainSection";
+import useTranslation from "../../hooks/useTranslation";
 import styles from "../../styles/About.module.css";
 
 export default function About() {
+  const { t, translation } = useTranslation();
+
   return (
     <>
       <div className={styles.sectionTop}>
         <Button>
-          <Link href="/">Go Home</Link>
+          <Link href={{ pathname: "/" }}>{t("goHome")}</Link>
         </Button>
       </div>
       <MainSection />
