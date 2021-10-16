562ca94d944d00ac992867364ba51de722dc4b33
Pol Reig <83955122+polrb5@users.noreply.github.com>
2021-10-16 12:33:25 +0200

    Call API to get listTranslations using createAsyncThunk

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { fetchTranslations } from "../src/redux/slices/translations";
+import { getTranslations } from "../src/redux/slices/translations";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import MainSection from "../components/MainSection/MainSection";
@@ -10,7 +10,7 @@ import { listTranslations } from "../src/graphql/queries";
 export default function Home({ tags }) {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchTranslations());
+    dispatch(getTranslations());
   }, [dispatch]);
 
   return (
--- src/redux/slices/translations.js
+++ src/redux/slices/translations.js
@@ -1,53 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { API } from "aws-amplify";
 import { listTranslations } from "../../graphql/queries";
+import { i18n } from "../../../next.config";
 
 export const initialState = {
   loading: false,
   hasErrors: false,
   translations: [],
+  lang: i18n.defaultLocale,
+  supportedLangs: i18n.locales,
 };
+
+export const getTranslations = createAsyncThunk(
+  "translations/getTransaltions",
+  async () => {
+    const {
+      data: {
+        listTranslations: { items },
+      },
+    } = await API.graphql({ query: listTranslations });
+    return items;
+  }
+);
 
 const translationsSlice = createSlice({
   name: "translations",
   initialState,
-  reducers: {
-    getTranslations: (state) => {
+  extraReducers: {
+    [getTranslations.pending]: (state) => {
       state.loading = true;
     },
-    getTranslationsSuccess: (state, { payload }) => {
+    [getTranslations.fulfilled]: (state, { payload }) => {
       state.translations = payload;
       state.loading = false;
       state.hasErrors = false;
     },
-    getTranslationsFailure: (state) => {
+    [getTranslations.rejected]: (state) => {
       state.loading = false;
       state.hasErrors = true;
     },
   },
 });
 
-export const {
-  getTranslations,
-  getTranslationsSuccess,
-  getTranslationsFailure,
-} = translationsSlice.actions;
-
 export default translationsSlice.reducer;
-
-export function fetchTranslations() {
-  return async (dispatch) => {
-    dispatch(getTranslations());
-    try {
-      const {
-        data: {
-          listTranslations: { items },
-        },
-      } = await API.graphql({ query: listTranslations });
-
-      dispatch(getTranslationsSuccess(items));
-    } catch (error) {
-      dispatch(getTranslationsFailure());
-    }
-  };
-}
